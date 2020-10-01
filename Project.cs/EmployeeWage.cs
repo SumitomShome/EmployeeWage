0b5a2955c38acada23e1c9dbf5d0cfcec82f70e3
SumitomShome <sumitomshome@gmail.com>
2020-10-01 18:23:26 +0530

    [Sumitom] Refactor: Commiting UC3-PartTime

--- Project.cs/EmployeeWage.cs
+++ Project.cs/EmployeeWage.cs
@@ -8,16 +8,21 @@ namespace Wage
              Console.WriteLine("Welcome to the Employee Wage Problem");
             //Constants
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
