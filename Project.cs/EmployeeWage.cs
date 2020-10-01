0503edd55192c8f169241f429edd7b3ac4530e00
SumitomShome <sumitomshome@gmail.com>
2020-10-01 19:00:10 +0530

    [Sumitom] Refactor: Commiting UC5-MonthlyWage

--- Project.cs/EmployeeWage.cs
+++ Project.cs/EmployeeWage.cs
@@ -6,7 +6,7 @@ namespace Wage
         public const int IS_FULL_TIME = 1;
         public const int IS_PART_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
-        int NUM_OF_WORKING_DAYS = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Problem");
@@ -17,7 +17,7 @@ namespace Wage
             //Computation
             for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                Random random = new Random;
+                Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
