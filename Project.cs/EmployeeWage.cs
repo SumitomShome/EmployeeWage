89d7eba858db0ba3c90d04b0099f51cf859e9b6c
SumitomShome <sumitomshome@gmail.com>
2020-10-04 19:55:06 +0530

    [Sumitom] Refactor: Commiting UC9-TotalWageMultipleCompanies

--- Project.cs/EmployeeWage.cs
+++ Project.cs/EmployeeWage.cs
@@ -3,47 +3,15 @@ namespace Wage
 {
     class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public static int ComputeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
             Console.WriteLine("Welcome to Employee Wage Problem");
-            //Variables
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            //Computation
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day#: " + totalWorkingDays + "  Emp Hrs: " + empHrs);
-            }
-
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("Total Emp Wage for Company: " +company+ " is " + totalEmpWage);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.toString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.toString());
+            Console.WriteLine(reliance.toString());
             Console.ReadKey();
-            return totalEmpWage;
-        }
-        static void Main(string[] args)
-        {
-            ComputeEmpWage("DMart", 20, 2, 10);
-            ComputeEmpWage("Reliance", 10, 4, 20);
         }
     }
 }
-
